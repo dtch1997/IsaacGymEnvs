c8ad7d08fce6d88fe9882a654f2e1036ab6a0af1
Daniel Tan <dtch1997@users.noreply.github.com>
2022-12-13 18:34:32 +0000

    Add logging of encoder loss

--- isaacgymenvs/cleanrl_ppo_url.py
+++ isaacgymenvs/cleanrl_ppo_url.py
@@ -447,6 +447,7 @@ if __name__ == "__main__":
         writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
         writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
         writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
+        writer.add_scalar("losses/encoder", encoder_loss.item(), global_step)
         writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
         writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
         writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
