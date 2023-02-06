90771a0a9f44ebb54f7196d32b785082a1b4140a
Daniel Tan <dtch1997@users.noreply.github.com>
2023-02-06 16:41:21 +0000

    Add training loop from AMPAgent

--- isaacgymenvs/learning/ase_agent.py
+++ isaacgymenvs/learning/ase_agent.py
@@ -57,6 +57,79 @@ class ASEAgent(amp_continuous.AMPAgent):
 
         return
 
+    def play_steps(self):
+        self.set_eval()
+
+        epinfos = []
+        update_list = self.update_list
+
+        for n in range(self.horizon_length):
+            self.obs, done_env_ids = self._env_reset_done()
+            self.experience_buffer.update_data('obses', n, self.obs['obs'])
+
+            if self.use_action_masks:
+                masks = self.vec_env.get_action_masks()
+                res_dict = self.get_masked_action_values(self.obs, masks)
+            else:
+                res_dict = self.get_action_values(self.obs)
+
+            for k in update_list:
+                self.experience_buffer.update_data(k, n, res_dict[k]) 
+
+            if self.has_central_value:
+                self.experience_buffer.update_data('states', n, self.obs['states'])
+
+            self.obs, rewards, self.dones, infos = self.env_step(res_dict['actions'])
+            shaped_rewards = self.rewards_shaper(rewards)
+            self.experience_buffer.update_data('rewards', n, shaped_rewards)
+            self.experience_buffer.update_data('next_obses', n, self.obs['obs'])
+            self.experience_buffer.update_data('dones', n, self.dones)
+            self.experience_buffer.update_data('amp_obs', n, infos['amp_obs'])
+
+            terminated = infos['terminate'].float()
+            terminated = terminated.unsqueeze(-1)
+            next_vals = self._eval_critic(self.obs)
+            next_vals *= (1.0 - terminated)
+            self.experience_buffer.update_data('next_values', n, next_vals)
+
+            self.current_rewards += rewards
+            self.current_lengths += 1
+            all_done_indices = self.dones.nonzero(as_tuple=False)
+            done_indices = all_done_indices[::self.num_agents]
+  
+            self.game_rewards.update(self.current_rewards[done_indices])
+            self.game_lengths.update(self.current_lengths[done_indices])
+            self.algo_observer.process_infos(infos, done_indices)
+
+            not_dones = 1.0 - self.dones.float()
+
+            self.current_rewards = self.current_rewards * not_dones.unsqueeze(1)
+            self.current_lengths = self.current_lengths * not_dones
+        
+            if (self.vec_env.env.viewer and (n == (self.horizon_length - 1))):
+                self._amp_debug(infos)
+
+        mb_fdones = self.experience_buffer.tensor_dict['dones'].float()
+        mb_values = self.experience_buffer.tensor_dict['values']
+        mb_next_values = self.experience_buffer.tensor_dict['next_values']
+
+        mb_rewards = self.experience_buffer.tensor_dict['rewards']
+        mb_amp_obs = self.experience_buffer.tensor_dict['amp_obs']
+        amp_rewards = self._calc_amp_rewards(mb_amp_obs)
+        mb_rewards = self._combine_rewards(mb_rewards, amp_rewards)
+
+        mb_advs = self.discount_values(mb_fdones, mb_values, mb_rewards, mb_next_values)
+        mb_returns = mb_advs + mb_values
+
+        batch_dict = self.experience_buffer.get_transformed_list(a2c_common.swap_and_flatten01, self.tensor_list)
+        batch_dict['returns'] = a2c_common.swap_and_flatten01(mb_returns)
+        batch_dict['played_frames'] = self.batch_size
+
+        for k, v in amp_rewards.items():
+            batch_dict[k] = a2c_common.swap_and_flatten01(v)
+
+        return batch_dict
+
     def _reset_latent_step_count(self, env_ids):
         self._latent_reset_steps[env_ids] = torch.randint_like(self._latent_reset_steps[env_ids], low=self._latent_steps_min, 
                                                          high=self._latent_steps_max)
