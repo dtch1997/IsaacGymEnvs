df934f4ff34b6c1bd40b6ad5a9a6fcc6ee97ab7d
Daniel Tan <dtch1997@users.noreply.github.com>
2023-02-23 10:38:36 +0000

    Enable visualization

--- isaacgymenvs/scripts/viz_dataset.py
+++ isaacgymenvs/scripts/viz_dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 import pathlib
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser("Visualize quadruped motion data")
+    parser = argparse.ArgumentParser("Visualize expert dataset")
     parser.add_argument("--filepath", type=str)
     args = parser.parse_args()
     return args
@@ -14,32 +14,32 @@ def line_plot(ax: plt.Axes, x: np.ndarray, y: np.ndarray, title: str, fontsize: 
     ax.plot(x, y)
     ax.set_title(title, size= fontsize)
 
-def plot_motion_data(ts, frames, frame_vels) -> plt.Axes:
-    fig, ax = plt.subplots(2, 3, figsize=(40, 20))
+def plot_motion_data(ts, root_states) -> plt.Axes:
+    fig, ax = plt.subplots(2, 2, figsize=(30, 20))
 
     # Body pos 
-    body_pos = frames[:, :3]
+    body_pos = root_states[:, :3]
     line_plot(ax[0][0], ts, body_pos, title="Body Pos")
 
     # Body orn
-    body_orn = frames[:, 3:7]
+    body_orn = root_states[:, 3:7]
     line_plot(ax[0][1], ts, body_orn, title="Body Orn")
 
-    # Dof pos 
-    dof_pos = frames[:, 7:]
-    line_plot(ax[0][2], ts, dof_pos, title="Dof Pos")
+    # Dof pos
+    # dof_pos = root_states[:, 7:10]
+    # line_plot(ax[0][2], ts, dof_pos, title="Dof Pos")
 
-    # Body lin vel
-    body_lin_vel = frame_vels[:, :3]
+    # Body ang vel
+    body_lin_vel = root_states[:, 10:]
     line_plot(ax[1][0], ts, body_lin_vel, title="Body Lin Vel")
 
     # Body ang vel
-    body_ang_vel = frame_vels[:, 3:6]
+    body_ang_vel = root_states[:, 3:6]
     line_plot(ax[1][1], ts, body_ang_vel, title="Body Ang Vel")
 
     # Dof vel
-    dof_vel = frame_vels[:, 6:]
-    line_plot(ax[1][2], ts, dof_vel, title="Dof Vel") 
+    # dof_vel = frame_vels[:, 6:]
+    # line_plot(ax[1][2], ts, dof_vel, title="Dof Vel") 
 
     return fig, ax 
 
@@ -51,8 +51,10 @@ if __name__ == "__main__":
     print(dataset['root_states'].attrs['size'])
     print(dataset['actions'].shape)
     print(dataset['actions'].attrs['size'])
-    # fig, ax = plot_motion_data(dataset['root_states'])
-    # filename = pathlib.Path(args.filepath).stem
-    # fig.suptitle(filename, size=30)
-    # fig.show()    
-    # input("Press any key to exit...")
+
+    ts = np.arange(200) * 0.02
+    fig, ax = plot_motion_data(ts, dataset['root_states'][0])
+    filename = pathlib.Path(args.filepath).stem
+    fig.suptitle(filename, size=30)
+    fig.show()    
+    input("Press any key to exit...")
