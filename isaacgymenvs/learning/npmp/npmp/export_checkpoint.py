5f6cbfdc18bb26cce799f087a62d3843db7ec061
Daniel Tan <dtch1997@users.noreply.github.com>
2023-02-24 11:34:37 +0000

    Enable CLI args for input, output filepaths

--- isaacgymenvs/learning/npmp/npmp/export_checkpoint.py
+++ isaacgymenvs/learning/npmp/npmp/export_checkpoint.py
@@ -1,12 +1,18 @@
 """ Export Pytorch Lightning checkpoint to vanilla PyTorch checkpoint """
 import torch
 import pathlib
+import argparse
 from npmp.model import Encoder, Actor
 from npmp.train import BehaviourCloning
 
 if __name__ == "__main__":
 
-    state_dim = 4 + 3 + 3 + 12 + 12 # root_rot, root_lin_vel, root_ang_vel, dof_pos, dof_vel
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-filepath", type=str)
+    parser.add_argument("-o", "--output-filepath", type=str, default=".")
+    args = parser.parse_args()
+
+    state_dim = 45
     action_dim = 12 # 12 dof pos
     latent_dim = 32
     num_future_states = 2
@@ -17,11 +23,12 @@ if __name__ == "__main__":
     actor = Actor(state_dim, action_dim, latent_dim, hidden_dim= hidden_dim)
     
     root_dir = pathlib.Path(__file__).absolute().parent.parent
-    checkpoint_path = root_dir / 'QuadrupedASE' / '7uoolrxw' / 'checkpoints' / 'epoch=49-step=71700.ckpt' 
     model = BehaviourCloning(encoder, actor)
     
-    checkpoint = torch.load(checkpoint_path)
+    checkpoint = torch.load(args.input_filepath)
     model.load_state_dict(checkpoint['state_dict'])
 
-    torch.save(model.encoder.state_dict(), 'encoder.pth')
-    torch.save(model.actor.state_dict(), 'actor.pth')
+    output_dir = pathlib.Path(args.output_filepath)
+    output_dir.mkdir(exist_ok=True, parents=True)
+    torch.save(model.encoder.state_dict(), output_dir / 'encoder.pth')
+    torch.save(model.actor.state_dict(), output_dir / 'actor.pth')
