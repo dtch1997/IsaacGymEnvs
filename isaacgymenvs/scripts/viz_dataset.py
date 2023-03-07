aa7a350f6d7a60e33b1d9ffed199765accc36071
Daniel Tan <dtch1997@users.noreply.github.com>
2023-03-07 11:18:41 +0000

    Refactor plotting function into separate module

--- isaacgymenvs/scripts/viz_dataset.py
+++ isaacgymenvs/scripts/viz_dataset.py
@@ -1,10 +1,9 @@
-import matplotlib.pyplot as plt
 import argparse
 import h5py
 import numpy as np
 import pathlib
 
-from typing import Optional, List
+from isaacgymenvs.utilities.viz_util import plot_motion_data
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser("Visualize expert dataset")
@@ -12,74 +11,6 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("-t", "--trajectory-idx", type=int, default=0)
     args = parser.parse_args()
     return args
-
-def line_plot(ax: plt.Axes, x: np.ndarray, y: np.ndarray, 
-              title: str, fontsize: int = 20,
-              xlabel: str = "", 
-              ylabel: str = "", 
-              labels: Optional[List[str]] = None):
-    ax.plot(x, y)
-    ax.set_title(title, size= fontsize)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    if labels is not None:
-        ax.legend(labels)
-
-def plot_motion_data(ts, root_states, dof_pos, dof_vel) -> plt.Axes:
-    fig, ax = plt.subplots(2, 3, figsize=(40, 20))
-    fig.set_tight_layout(True)
-
-    # Body pos 
-    body_pos = root_states[:, :3]
-    line_plot(ax[0][0], ts, body_pos, 
-            title="Body Pos", 
-            xlabel="Time (s)",
-            ylabel="Position (m)",
-            labels = ["x", "y", "z"]
-    )
-
-    # Body orn
-    body_orn = root_states[:, 3:7]
-    line_plot(ax[0][1], ts, body_orn, 
-            title="Body Orn",  
-            xlabel="Time (s)", 
-            labels = ["qx", "qy", "qz", "qw"]
-    )
-
-    # Dof pos
-    line_plot(ax[0][2], ts, dof_pos, 
-            title="Dof Pos",  
-            xlabel="Time (s)",
-            ylabel="Joint pos (rad)"
-    )
-
-    # Body ang vel
-    body_lin_vel = root_states[:, 7:10]
-    line_plot(ax[1][0], ts, body_lin_vel, 
-            title="Body Lin Vel",  
-            xlabel="Time (s)",
-            ylabel="Velocity (m/s)",
-            labels=["dx", "dy", "dz"]
-    )
-
-    # Body ang vel
-    body_ang_vel = root_states[:, 10:]
-    line_plot(ax[1][1], ts, body_ang_vel, 
-            title="Body Ang Vel",  
-            xlabel="Time (s)", 
-            ylabel="Ang vel (rad/s)",
-            labels=["dR", "dP", "dY"]
-    )
-
-    # Dof vel
-    line_plot(ax[1][2], ts, dof_vel, 
-            title="Dof Vel",  
-            xlabel="Time (s)",
-            ylabel="Joint vel (rad/s)"
-              
-    ) 
-
-    return fig, ax 
 
 if __name__ == "__main__":
 
