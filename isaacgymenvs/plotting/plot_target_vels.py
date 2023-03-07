66de02cf0d4eccb7ee03bdd53f3a8adb7b0db501
Daniel Tan <dtch1997@users.noreply.github.com>
2023-03-07 11:41:12 +0000

    Add script to plot target vels

--- isaacgymenvs/plotting/plot_target_vels.py
+++ isaacgymenvs/plotting/plot_target_vels.py
@@ -1,10 +1,7 @@
-import matplotlib.pyplot as plt
 import argparse
 import h5py
 import numpy as np
-import pathlib
-
-from typing import Optional, List
+import matplotlib.pyplot as plt
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser("Visualize expert dataset")
@@ -12,89 +9,52 @@ def parse_args() -> argparse.Namespace:
     args = parser.parse_args()
     return args
 
-def line_plot(ax: plt.Axes, x: np.ndarray, y: np.ndarray, 
-              title: str, fontsize: int = 20,
-              xlabel: str = "", 
-              ylabel: str = "", 
-              labels: Optional[List[str]] = None):
-    ax.plot(x, y)
-    ax.set_title(title, size= fontsize)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    if labels is not None:
-        ax.legend(labels)
-
-def plot_motion_data(ts, root_states, dof_pos, dof_vel) -> plt.Axes:
-    fig, ax = plt.subplots(2, 3, figsize=(40, 20))
-    fig.set_tight_layout(True)
-
-    # Body pos 
-    body_pos = root_states[:, :3]
-    line_plot(ax[0][0], ts, body_pos, 
-            title="Body Pos", 
-            xlabel="Time (s)",
-            ylabel="Position (m)",
-            labels = ["x", "y", "z"]
-    )
-
-    # Body orn
-    body_orn = root_states[:, 3:7]
-    line_plot(ax[0][1], ts, body_orn, 
-            title="Body Orn",  
-            xlabel="Time (s)", 
-            labels = ["qx", "qy", "qz", "qw"]
-    )
-
-    # Dof pos
-    line_plot(ax[0][2], ts, dof_pos, 
-            title="Dof Pos",  
-            xlabel="Time (s)",
-            ylabel="Joint pos (rad)"
-    )
-
-    # Body ang vel
-    body_lin_vel = root_states[:, 7:10]
-    line_plot(ax[1][0], ts, body_lin_vel, 
-            title="Body Lin Vel",  
-            xlabel="Time (s)",
-            ylabel="Velocity (m/s)",
-            labels=["dx", "dy", "dz"]
-    )
-
-    # Body ang vel
-    body_ang_vel = root_states[:, 10:]
-    line_plot(ax[1][1], ts, body_ang_vel, 
-            title="Body Ang Vel",  
-            xlabel="Time (s)", 
-            ylabel="Ang vel (rad/s)",
-            labels=["dR", "dP", "dY"]
-    )
-
-    # Dof vel
-    line_plot(ax[1][2], ts, dof_vel, 
-            title="Dof Vel",  
-            xlabel="Time (s)",
-            ylabel="Joint vel (rad/s)"
-              
-    ) 
-
-    return fig, ax 
-
 if __name__ == "__main__":
 
     args = parse_args()
     file = h5py.File(args.filepath)
     for key, value in file.attrs.items():
         print(f"{key}: {value}")
-    for name in file.keys():
-        print(name)
-        print(file[name].shape)
-        print(file[name].attrs['size'])
 
-    ts = np.arange(file.attrs['max_episode_length']) * file.attrs['dt']
+    tensors = {}
+    for name in file.keys():
+        size = file[name].attrs['size']
+        # Remove last index due to reset
+        tensors[name] = file[name][:size][:, :-1, :]
+        print(name, size)
+        print(tensors[name].shape)
+
+    # Scatter plot of target velocity vs average followed velocity
+    root_states = tensors['root_states']
+    root_lin_vel = root_states[:, :, 7:10]
+    dt = file.attrs['dt']
+
+    task_state = tensors['task_state']
+    # Sanity check: task state should be constant throughout episode
+    assert np.all(task_state == task_state[:, 0:1, :])
+
+    # Calculate target vel
+    target_direction = task_state[:, 0, :3]
+    target_speed = task_state[:, 0, -1:]
+    target_vel = target_direction * target_speed
+    # shape (N, 3)
+
+    # Calculate average vel 
+    start_root_pos = root_states[:, 0, :3]
+    end_root_pos = root_states[:, -1, :3]
+    total_time = dt * root_states.shape[1]
+    average_vel = (end_root_pos - start_root_pos) / total_time
+    # shape (N, 3)
+
+
+    # Calculate dot-product
+    dot_prod = np.sum(target_vel * average_vel, axis=-1)
+    # shape (N,)
+
+    # Plot histogram of fits
+    fig, ax = plt.subplots()
+    ax.hist(dot_prod, bins=20)
+    fig.show()
+    input("Press any key to exit...")
+
     
-    # Calculate motion statistics
-    traj_idx = args.trajectory_idx
-    root_states = file['root_states'][traj_idx]
-    dof_pos = file['dof_pos'][traj_idx]
-    dof_vel = file['dof_vel'][traj_idx]
