da3c532919b008d73dacdd5316b3eb324bcff95f
Daniel Tan <dtch1997@users.noreply.github.com>
2023-06-01 16:56:03 +0100

    Modify experiment commanD

--- experiments/mania_dataset.sh
+++ experiments/mania_dataset.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-GROUP="mania"
-for motionIdx in 3 6 8
+GROUP="mania_2"
+for motionIdx in 1 2 3 4 6 7 8
 do
-    for temperature in 10.0 1.0 0.1
-    do
-        python train.py \
-            task=QuadrupedAMP \
-            task.env.motionFile=data/motions/quadruped/mania/motion$motionIdx.txt \
-            headless=True \
-            wandb_entity=dtch1997 \
-            wandb_project=QuadrupedASE \
-            wandb_activate=True \
-            wandb_group=$GROUP \
-            max_iterations=2000 \
-            wandb_name=mania${motionIdx}_temp${temperature} \
-            train.params.config.disc_temperature=$temperature
-    done
+    python train.py \
+        task=QuadrupedAMP \
+        task.env.motionFile=data/motions/quadruped/mania/motion$motionIdx.txt \
+        task.env.urdfAsset.filepath=urdf/a1_mania.urdf \
+        headless=True \
+        wandb_entity=dtch1997 \
+        wandb_project=QuadrupedASE \
+        wandb_activate=True \
+        wandb_group=$GROUP \
+        max_iterations=2000 \
+        wandb_name=mania${motionIdx}
 done
