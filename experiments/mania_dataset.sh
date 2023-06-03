6298ab9bd5b72fd9735865b4dd73d020a2817a7b
Daniel Tan <dtch1997@users.noreply.github.com>
2023-06-03 10:12:11 +0100

    Modify experimental setup

--- experiments/mania_dataset.sh
+++ experiments/mania_dataset.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-GROUP="mania_2"
+GROUP=${1:-"mania_2"}
 for motionIdx in 1 2 3 4 6 7 8
 do
     python train.py \
         task=QuadrupedAMP \
         task.env.motionFile=data/motions/quadruped/mania/motion$motionIdx.txt \
         task.env.urdfAsset.filepath=urdf/a1_mania.urdf \
+        task.env.enableEarlyTermination=True \
+        task.env.enableRefStateInitHeight=True \
+        train.params.network.space.continuous.learn_sigma=True \
         headless=True \
         wandb_entity=dtch1997 \
         wandb_project=QuadrupedASE \
         wandb_activate=True \
         wandb_group=$GROUP \
-        max_iterations=2000 \
+        max_iterations=5000 \
         wandb_name=mania${motionIdx}
 done
